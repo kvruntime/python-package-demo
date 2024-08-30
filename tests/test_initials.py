819c1b934fc89121a09987899a0518a11e00b7a8
kvruntime <kvruntime@gmail.com>
2024-08-30 17:06:55 +0000

    complete challenge01

--- tests/test_initials.py
+++ tests/test_initials.py
@@ -4,10 +4,21 @@ _context.__file__
 from demopack import functions
 
 
-def test__calculate_energy__take_200_10_expected_200():
+def test__calculate_energy__take_100_2_expected_200():
     power = 100
     hours = 2
     expected = 200
+
+    predicate = functions.calculate_energy(power, hours)
+
+    assert predicate == expected
+    return None
+
+
+def test__calculate_energy__take_0_10_expected_0():
+    power = 0
+    hours = 2
+    expected = 0
 
     predicate = functions.calculate_energy(power, hours)
 

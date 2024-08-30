bd031c4eaba1b43c5de453cb5b1b998fa4c89703
kvruntime <kvruntime@gmail.com>
2024-08-30 16:27:06 +0000

    fix testing errors

--- tests/_context.py
+++ tests/_context.py
@@ -3,9 +3,7 @@ from pathlib import Path
 
 
 def register_package():
-    sys.path.insert(
-        0, Path(__file__).parent.parent.joinpath("src", "ingineerpackage").as_posix()
-    )
+    sys.path.insert(0, Path(__file__).parent.parent.joinpath("src").as_posix())
     import demopack
 
     demopack.__file__
